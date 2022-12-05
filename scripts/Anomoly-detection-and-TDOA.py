01e4be3f921a156cc4a759463404466f8e2c13d0
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2022-12-05 11:55:53 +0100

    redid the TDOA algorithm and commented unused libraries

--- scripts/Anomoly-detection-and-TDOA.py
+++ scripts/Anomoly-detection-and-TDOA.py
@@ -1,14 +1,15 @@
+#Import libraries
 import sounddevice as sd
-from scipy.io.wavfile import write
+#from scipy.io.wavfile import write
 from scipy import signal
 import numpy as np
-import librosa
-from librosa import display
-import matplotlib.pyplot as plt
-from playsound import playsound
-import cv2 as cv
-import time
-from time import sleep
+#import librosa
+#from librosa import display
+#import matplotlib.pyplot as plt
+#from playsound import playsound
+#import cv2 as cv
+#import time
+#from time import sleep
 
 #A duration for each sound sample along with a sampling rate
 #This should be the same duration used when making the template
@@ -76,30 +77,97 @@ def TDOA(signal1, signal2):
     tdoa = (u-sampling_rate)/sampling_rate
     return tdoa
 
-class TriangularMicArr:
+class Triangular_mic_array:
     def __init__(self,d):
         self.d = d
-    def findSoundAngle(self,t12,t13,t23):
+    
+    def __find_angle_difference(self,ref_angle_rad, time_difference_seconds, time_id):
+        """finds the angle closest to the reference angle
+            ### PRIVATE FUNCTION ###
+        Args:
+            ref_angle (float): angle expected to be close to the correct angle
+            time_difference_seconds (float): time difference between sound wave arrivals (seconds)
+            time_id (String): id of the time difference
+
+        Returns:
+            float: angle given the time difference (radians)
+        """
+        if time_id == 't12': factor = -1
+        elif time_id=='t13': factor =  1
+        else:
+            print('Incorrect input for time_id; Expected \'t12\' or \'t13\', recieved ', time_id)
+            return 'Error'
+        
+        angle_1 = factor*(np.pi/6-np.arccos(c/self.d*time_difference_seconds))
+        angle_2 = factor*(np.arccos(c/self.d*time_difference_seconds)-np.pi*11/6)
+        if abs(angle_2)> np.pi:
+            angle_2 = factor*(np.arccos(c/self.d*time_difference_seconds)+np.pi/6)
+         
+        ## Print statement for testing
+        #print('\nTime difference: ',time_id,'\nAngle_1: ', angle_1,'\nAngle_2: ',angle_2)
+        
+        angle_difference_1 = abs(angle_1-ref_angle_rad)
+        if angle_difference_1 > np.pi:
+            angle_difference_1 = 2*np.pi-angle_difference_1
+            
+        angle_difference_2 = abs(angle_2-ref_angle_rad)
+        if angle_difference_2 > np.pi:
+            angle_difference_2 = 2*np.pi-angle_difference_2
+        
+        if angle_difference_1 < angle_difference_2:
+            return_angle_rad = angle_1
+        else:
+            return_angle_rad = angle_2
+        #if return_angle_rad < 0:
+        #    return return_angle_rad + 2*np.pi - ref_angle_rad
+        #else:
+        angle_difference = return_angle_rad - ref_angle_rad
+        if angle_difference < -np.pi:
+            return angle_difference + 2*np.pi
+        elif angle_difference > np.pi:
+            return angle_difference - 2*np.pi
+        else:
+            return angle_difference
+        
+    def find_sound_angle(self,t12,t13,t23):
         a1 = np.arcsin(c*t23/self.d)
-        if a1 >= 0:
-            a2 = np.pi-a1
+        if a1>=0:
+            a2 =  np.pi - a1
         else:
-            a2 = -np.pi-a1
-        a=[a1,a2]
-        t12Expected = [self.d/c*np.cos(a1+np.pi/6),self.d/c*np.cos(a2+np.pi/6)]
-        t13Expected = [self.d/c*np.cos(a1-np.pi/6),self.d/c*np.cos(a2-np.pi/6)] 
-        if abs(t12-t12Expected[0]) < abs(t12-t12Expected[1]):
-            t12ExpectedIndex=0
+            a2 = -np.pi - a1
+        a = [a1,a2]
+        t12_expected = [self.d/c*np.cos(angle + np.pi/6) for angle in a]
+        t13_expected = [self.d/c*np.cos(angle - np.pi/6) for angle in a]
+        
+        differences_1 = [abs(t12-time_difference) for time_difference in t12_expected]
+        differences_2 = [abs(t13-time_difference) for time_difference in t13_expected]
+        
+        if differences_1[0] < differences_1[1]:
+            index_1 = 0
         else:
-            t12ExpectedIndex=1
-        if abs(t13-t13Expected[0]) < abs(t13-t13Expected[1]):
-            t13ExpectedIndex=0
+            index_1 = 1
+        if differences_2[0] < differences_2[1]:
+            index_2 = 0
         else:
-            t13ExpectedIndex=1
-        if t12ExpectedIndex == t13ExpectedIndex:
-            return a[t12ExpectedIndex]
-        else:
-            print("An error has occured:\n Angles not same\n")
+            index_2 = 1
+            
+        if index_1 != index_2:
+            print('\nTime difference error! Time differences impossible.\n')
+            return 'error'
+        angle_1 = a[index_1]
+        
+        angle_difference_2 = self.__find_angle_difference(angle_1,t12,'t12')
+        angle_difference_3 = self.__find_angle_difference(angle_1,t13,'t13')
+        
+        #print('\nAngle_1:\n',angle_1,'\nAngle differences:\n',angle_difference_2,'\n',angle_difference_3)
+        
+        average_angle = angle_1 + (angle_difference_2+angle_difference_3)/3
+        
+        if average_angle > np.pi:
+            return average_angle - 2*np.pi
+        if average_angle < -np.pi:
+            return average_angle + 2*np.pi
+        return average_angle
 
 
 i = 0
@@ -117,7 +185,7 @@ while i<reps:
         t12 = TDOA(rec1,rec2)
         t23 = TDOA(rec2,rec3)
         t13 = TDOA(rec1,rec3)
-        tri = TriangularMicArr(d)
+        tri = Triangular_mic_array(d)
         #rad = tri.findSoundAngle(t12,t13,t23)
         #print("rad: " + str(rad))
         #angle = rad*(180/np.pi)
